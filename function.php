3bf049d68a7cbbebb238a7f0a08756401d1e7997
Vũ Hoàng Dương <38931994+binmadie@users.noreply.github.com>
2020-12-29 17:29:25 +0700

    Update function.php

--- function.php
+++ function.php
@@ -10,3 +10,14 @@ function delete_all_attached_media( $post_id ) {
 }
 // Khung soạn thảo cơ bản trong wordpress
 add_filter('use_block_editor_for_post', '__return_false');
+
+// Custom Fields For Builder Blog Layout
+function post_title_shortcode(){
+    return get_the_title();
+}
+add_shortcode('post_title','post_title_shortcode');
+
+function post_content_shortcode(){
+    return get_the_content();
+}
+add_shortcode('post_content','post_content_shortcode');
